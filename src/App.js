bc2ade1e5323770edc811cd5b3b5567f781235b8
yemiOdetola <yemiotola@outlook.com>
2018-11-01 10:11:57 +0100

    Its Lady-R birthMonth, this push for her, n her alone

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layouts/Layout';
+import BreadBuilder from './containers/BreadBuilder/BreadBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Layout>
+          <BreadBuilder />
+        </Layout>
       </div>
     );
   }
---
+++ src/containers/BreadBuilder/BreadBuilder.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    // constructor() {
+    //     super()
+    // }
+
+    render() {
+        return (
+            <Aux>
+                <div>Dish</div>
+                <div>Build Controls</div>
+            </Aux>
+        )
+    }
+
+}
+
+export default BurgerBuilder;
